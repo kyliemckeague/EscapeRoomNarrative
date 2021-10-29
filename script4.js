3cfe7e0b0e9f931b8edb31d1a4bd60188427751d
kyliemckeague <89169193+kyliemckeague@users.noreply.github.com>
2021-10-29 12:12:43 -0400

    Update script4.js

--- script4.js
+++ script4.js
@@ -1,8 +1,8 @@
 //the actual password and where to change it if needed//
 function checkPassword() {
    var password = document.getElementById("passwordBox");
-   var passwordText = password.value;
-   if(passwordText == "Dali") {
+    var passwordText = password.value.toUpperCase();
+   if(passwordText == "DALI") {
     return true;
    }
    alert("Access denied! Incorrect password!");
